0cc0b1b5222eeadcbe3abbd03afdc86e3bfb8b32
Abdus Samee <abdussamee16@gmail.com>
2023-07-14 23:12:20 +0600

    Added dragable component in course content creation

--- src/components/AddCourseContent.jsx
+++ src/components/AddCourseContent.jsx
@@ -1,20 +1,41 @@
-import { useContext } from "react"
+import { useState, useEffect, useContext } from "react"
 import { Button, TextField } from "@mui/material"
 
 import { multiStepContext } from "../StepContext"
 
+import "../assets/css/coursecontent.css"
+
 const AddCourseContent = () => {
+    const [items, setItems] = useState([])
     const { setCurrentStep, userData, setUserData } = useContext(multiStepContext)
+
+    useEffect(() => {
+        console.log(items)
+    }, [items])
+
+    const handleDragStart = (event, type) => {
+
+    }
+      
+    const allowDrop = (event) => {
+        event.preventDefault()
+    }
+      
+    const handleDrop = (event) => {
+        event.preventDefault();
+
+        const newItem = "text"
+        setItems((prevItems) => [...prevItems, newItem])
+    }
 
     return (
         <div>
-            <div>
-                <TextField
-                    label="Course Content"
-                    margin="normal"
-                    variant="outlined"
-                    color="secondary"
-                />
+            <div className="course-container" onDrop={handleDrop} onDragOver={allowDrop}>
+                {items.map((item, key) => (
+                    <div className="course-item" key={key}>
+                        {item === "text" && <div><TextField margin="normal" variant="outlined" color="secondary" /></div>}
+                    </div>
+                ))}
             </div>
             <div>
                 <Button variant="contained" color="secondary" onClick={() => setCurrentStep(1)}>
@@ -24,6 +45,12 @@ const AddCourseContent = () => {
                 <Button variant="contained" color="primary" onClick={() => setCurrentStep(3)}>
                     Next
                 </Button>
+            </div>
+            <div className="content-panel">
+                <h3>Add Content</h3>
+                <div className="content-item" draggable="true" onDragStart={handleDragStart}>
+                    Text Box
+                </div>
             </div>
         </div>
     )
