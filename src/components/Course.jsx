805a6e73d088a412b1fec70b7b1285b5e4a25e05
PrinceZarzees <1805009@ugrad.cse.buet.ac.bd>
2023-07-31 22:15:18 +0600

    Add course cards and unique IDs

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,9 +1,58 @@
-const Course = () => {
-    return (
-        <div>
-            <h1>Courses</h1>
-        </div>
-    )
-}
+import React, { useState } from "react";
+import "../assets/css/course.css";
 
-export default Course
+const Course = () => {
+    const [courses, setCourses] = useState([
+      { id: 1, name: "Dynamic Programming" },
+      { id: 2, name: "Searching" },
+      { id: 3, name: "Sorting" }
+    ]);
+  
+    const addCourse = (name) => {
+      // Generate a unique ID for the new course
+      const newId = Math.max(...courses.map((course) => course.id)) + 1;
+      const newCourse = { id: newId, name };
+      setCourses((prevCourses) => [...prevCourses, newCourse]);
+    };
+  
+    return (
+      <div>
+        <h1>Courses</h1>
+        <div className="course-cards">
+          {courses.map((course) => (
+            <div className="course-card" key={course.id}>
+              <h2>{course.name}</h2>
+              {/* Add more course details or actions here if needed */}
+            </div>
+          ))}
+        </div>
+        <AddCourseForm addCourse={addCourse} />
+      </div>
+    );
+  };
+  
+  const AddCourseForm = ({ addCourse }) => {
+    const [courseName, setCourseName] = useState("");
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (courseName.trim() !== "") {
+        addCourse(courseName.trim());
+        setCourseName("");
+      }
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={courseName}
+          onChange={(e) => setCourseName(e.target.value)}
+          placeholder="Enter course name"
+        />
+        <button type="submit">Add Course</button>
+      </form>
+    );
+  };
+  
+  export default Course;
