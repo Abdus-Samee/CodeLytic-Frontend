c4b531babb6dc1ce4ed32176c2fd2adb3c57bfd4
PrinceZarzees <1805009@ugrad.cse.buet.ac.bd>
2023-08-05 19:36:14 +0600

    Add course mui cards

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,58 +1,31 @@
-import React, { useState } from "react"
-import "../assets/css/course.css"
+import React, { useState } from "react";
+import { Card, CardContent, Typography, TextField, Button } from "@mui/material";
+import "../assets/css/course.css";
 
 const Course = () => {
-    const [courses, setCourses] = useState([
-      { id: 1, name: "Dynamic Programming" },
-      { id: 2, name: "Searching" },
-      { id: 3, name: "Sorting" }
-    ]);
-  
-    const addCourse = (name) => {
-      // Generate a unique ID for the new course
-      const newId = Math.max(...courses.map((course) => course.id)) + 1
-      const newCourse = { id: newId, name }
-      setCourses((prevCourses) => [...prevCourses, newCourse])
-    };
-  
-    return (
-      <div>
-        <h1>Courses</h1>
-        <div className="course-cards">
-          {courses.map((course) => (
-            <div className="course-card" key={course.id}>
-              <h2>{course.name}</h2>
+  const [courses, setCourses] = useState([
+    { id: 1, name: "Dynamic Programming" },
+    { id: 2, name: "Searching" },
+    { id: 3, name: "Sorting" }
+  ]);
+
+  return (
+    <div>
+      <h1>Courses</h1>
+      <div className="course-cards">
+        {courses.map((course) => (
+          <Card className="course-card" key={course.id}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {course.name}
+              </Typography>
               {/* Add more course details or actions here if needed */}
-            </div>
-          ))}
-        </div>
-        <AddCourseForm addCourse={addCourse} />
+            </CardContent>
+          </Card>
+        ))}
       </div>
-    )
-  }
-  
-  const AddCourseForm = ({ addCourse }) => {
-    const [courseName, setCourseName] = useState("")
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (courseName.trim() !== "") {
-        addCourse(courseName.trim())
-        setCourseName("")
-      }
-    };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={courseName}
-          onChange={(e) => setCourseName(e.target.value)}
-          placeholder="Enter course name"
-        />
-        <button type="submit">Add Course</button>
-      </form>
-    );
-  };
-  
-  export default Course;
+    </div>
+  );
+};
+
+export default Course;
