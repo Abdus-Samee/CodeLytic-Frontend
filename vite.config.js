e8b273169f41389465d2db09de9b9345b15a9ef7
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-08-17 05:19:42 +0600

    Cleaned

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { writeFileSync, mkdirSync, existsSync } from 'fs'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    {
-      name: 'generate-redirects',
-      buildEnd: () => {
-        const redirectsContent = `
-        /* /index.html 200
-        `;
-
-        const redirectsFilePath = resolve(__dirname, 'dist', '_redirects');
-
-        if (!existsSync('dist')) {
-          mkdirSync('dist');
-        }
-
-        writeFileSync('dist/_redirects', redirectsContent);
-      },
-    },
   ],
 })
